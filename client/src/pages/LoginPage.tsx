8882c9cf1c7159bae510d64fca548277e128535b
killuaJ2002 <kjcodes2002@gmail.com>
2025-10-28 22:57:17 +0530

    form handling added

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -2,7 +2,24 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 const LoginPage = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = () => {
+    console.log(formData);
+  };
+
   return (
     <div className="flex flex-col gap-3">
       <h2 className="text-mainTextDark text-xl font-semibold mb-2">
@@ -11,14 +28,30 @@ const LoginPage = () => {
       <div className="flex flex-col gap-3">
         <div className="grid w-full max-w-sm items-center gap-3">
           <Label htmlFor="email">Email</Label>
-          <Input type="email" id="email" placeholder="" />
+          <Input
+            type="email"
+            id="email"
+            name="email"
+            placeholder=""
+            onChange={handleChange}
+          />
         </div>
         <div className="grid w-full max-w-sm items-center gap-3">
           <Label htmlFor="email">Password</Label>
-          <Input type="password" id="password" placeholder="" />
+          <Input
+            type="password"
+            id="password"
+            name="password"
+            placeholder=""
+            onChange={handleChange}
+          />
         </div>
       </div>
-      <Button variant="outline" className="max-w-16 text-mainTextDark">
+      <Button
+        variant="outline"
+        className="max-w-16 text-mainTextDark"
+        onClick={handleSubmit}
+      >
         Log In
       </Button>
       <p className="text-mainTextLight">
--- client/src/pages/SignupPage.tsx
+++ client/src/pages/SignupPage.tsx
@@ -2,7 +2,25 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 const SignupPage = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSumbit = () => {
+    console.log(formData);
+  };
+
   return (
     <div className="flex flex-col gap-3">
       <h2 className="text-mainTextDark text-xl font-semibold mb-2">
@@ -11,18 +29,40 @@ const SignupPage = () => {
       <div className="flex flex-col gap-3">
         <div className="grid w-full max-w-sm items-center gap-3">
           <Label htmlFor="email">Email</Label>
-          <Input type="email" id="email" placeholder="" />
+          <Input
+            type="email"
+            id="email"
+            name="email"
+            placeholder=""
+            onChange={handleChange}
+          />
         </div>
         <div className="grid w-full max-w-sm items-center gap-3">
           <Label htmlFor="password">Password</Label>
-          <Input type="password" id="password" placeholder="" />
+          <Input
+            type="password"
+            id="password"
+            name="password"
+            placeholder=""
+            onChange={handleChange}
+          />
         </div>
         <div className="grid w-full max-w-sm items-center gap-3">
           <Label htmlFor="confirmPassword">Confirm Password</Label>
-          <Input type="password" id="password" placeholder="" />
+          <Input
+            type="password"
+            id="password"
+            name="confirmPassword"
+            placeholder=""
+            onChange={handleChange}
+          />
         </div>
       </div>
-      <Button variant="outline" className="max-w-16 text-mainTextDark">
+      <Button
+        variant="outline"
+        className="max-w-16 text-mainTextDark"
+        onClick={handleSumbit}
+      >
         Sign Up
       </Button>
       <p className="text-mainTextLight">
